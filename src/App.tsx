f7d0823817d125104f8d06dcb51fcc51179b3580
Vanehezhewen <zhewen.he@mail.mcgill.ca>
2026-05-08 17:29:26 +0800

    comment in FAQ

--- src/App.tsx
+++ src/App.tsx
@@ -490,7 +490,7 @@ const CaseStudies = () => (
   </section>
 );
 
-/*
+
 const FAQ = () => (
   <section className="py-32 px-6 xl:px-[150px] bg-[#f5f5f7]">
     <div className="max-w-4xl mx-auto w-full">
@@ -544,7 +544,7 @@ const FAQ = () => (
     </div>
   </section>
 );
-*/
+
 
 const FinalCTA = () => (
   <section className="py-32 px-6 xl:px-[150px] bg-brand-accent text-white text-center flex flex-col items-center">
